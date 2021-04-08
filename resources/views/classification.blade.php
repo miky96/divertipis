d1d757d9820b1f99c4dc8220dd0256f26a7bfd4c
Miquel Freixes <mikyfreixes@gmail.com>
2021-04-08 17:34:29 +0200

    Added all new files as first commit

---
+++ resources/views/classification.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Landing - Forty by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+</head>
+    <body class="is-preload">
+    <div class="table-wrapper">
+        <table>
+            <thead>
+            <tr>
+                <th>Position</th>
+                <th>Guest name</th>
+                <th>Points</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Item1</td>
+                    <td>Ante turpis integer aliquet porttitor.</td>
+                    <td>29.99</td>
+                </tr>
+                <tr>
+                    <td>Item2</td>
+                    <td>Vis ac commodo adipiscing arcu aliquet.</td>
+                    <td>19.99</td>
+                </tr>
+                <tr>
+                    <td>Item3</td>
+                    <td> Morbi faucibus arcu accumsan lorem.</td>
+                    <td>29.99</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </body>
+</html>
